2bc4c11d1be6089d19eda9dba0a2551c74b67c62
Francisco Dias <72150861+Kik0H@users.noreply.github.com>
2025-04-14 16:23:45 +0100

    Primeiro Commit

---
+++ src/CarRental.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CarRental {
+    private Map<String, Carro> carros;
+
+    public CarRental(){
+        this.carros = new HashMap<>();
+    }
+
+    public CarRental (Map<String, Carro> carros){
+        //??!?!?!?
+    }
+
+}
